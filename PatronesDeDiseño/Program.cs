50b7721625017fe687b946f5bf996f6b81957c71
SantiGO055 <santigonzalez05@gmail.com>
2024-12-01 20:13:20 -0300

    Prototype Shallow (Superficial)

--- PatronesDeDiseño/Program.cs
+++ PatronesDeDiseño/Program.cs
@@ -1,7 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using PatronesDeDiseño.Prototype;
 using PatronesDeDiseño.Singleton;
 
-Console.WriteLine(Singleton.Instance.GetMessage());
-Console.WriteLine(Singleton.Instance.SetMessage("pepito"));
-Console.WriteLine(Singleton.Instance.GetMessage());
+#region Singleton
+// Console.WriteLine(Singleton.Instance.GetMessage());
+// Console.WriteLine(Singleton.Instance.SetMessage("pepito"));
+// Console.WriteLine(Singleton.Instance.GetMessage());
+#endregion
+
+#region Prototype
+Animal oAnimal = new Animal{ Nombre = "Oveja" , Patas = 4};
+Animal oAnimalClonado = oAnimal.Clone() as Animal;
+oAnimalClonado.Patas = 5;
+Console.WriteLine(oAnimalClonado.Patas);
+Console.WriteLine(oAnimal.Patas);
+#endregion
---
+++ PatronesDeDiseño/Prototype/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace PatronesDeDiseño.Prototype;
+
+public class Animal : ICloneable
+{
+    public int Patas { get; set; }
+    public string Nombre { get; set; }
+    
+    //Clonacion superficial - shallow: Clona solo atributos de esta clase basicos no objetos complejos
+    
+    public object Clone()
+    {
+        return MemberwiseClone();
+    }
+}
