854977e84dd4162c46a086ecc7651b0b2f916c7a
SantiGO055 <santigonzalez05@gmail.com>
2024-12-01 19:37:19 -0300

    first commit

---
+++ PatronesDeDiseño/Singleton/Singleton.cs
@@ -0,0 +1,8 @@
+﻿namespace PatronesDeDiseño.Singleton;
+
+public class Singleton
+{
+    private static Singleton? _instance;
+    
+    public static Singleton Instance => _instance ??= new Singleton();
+}
